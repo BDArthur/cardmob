57b62be0569c6b764b73788304db48f20fc203f1
arthur guerra <arthurguerraanastacio0@gmail.com>
2025-08-26 10:02:20 -0300

    Navegação logada e não logada

--- CARDMOB/etapa3/loja-react-app/App.tsx
+++ CARDMOB/etapa3/loja-react-app/App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { ThemeProvider } from './src/contexts/ThemeContext';
-import HomeScreen from './src/screens/HomeScreen';
+import RootNavigator from './src/navigation/RootNavigator';
+import { AuthProvider } from './src/contexts/AuthContext';
+
 
 export default function App() {
   return (
     <ThemeProvider>
-      <HomeScreen/>
+      <RootNavigator />
+      <AuthProvider>
+        <RootNavigator />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
