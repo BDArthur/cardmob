2c57da54e320998c1fa2db0ee11475fb32c61439
Arthur Guerra <arthurguerraanastacio0@gmail.com>
2025-11-11 10:14:04 -0300

    Conclusão da tela de checkout

--- CARDMOB/etapa3/loja-react-app/src/service/catalogService.ts
+++ CARDMOB/etapa3/loja-react-app/src/service/catalogService.ts
@@ -1,18 +1,45 @@
-const API_URL = 'http://10.81.205.50:5000';
+import Constants from 'expo-constants'; // novo
 
+const { apiUrl } = Constants.expoConfig?.extra || {}; // novo
 
-export async function getCatalog(): Promise<any> {
 export async function getCatalog(): Promise<any[]> { // alterado
     try {
-        const response = await fetch(`${API_URL}/api/catalog`);
+        // alterado
+        const response = await fetch(`${apiUrl}/api/catalog`);
         const data = await response.json();
         // console.log(data);
-        return Promise.resolve(data.catalog);
         // return Promise.resolve(data.catalog);
         return data.catalog; // incluido / alterado
     }
     catch (error) {
         console.error(error);
         return Promise.reject('Erro ao obter produtos');
+    }
+}
+
+export async function postOrder(customer: any, cartItems: any):Promise<any[]> {
+    try {
+        // enviar para o backend.
+        const orderData = {...customer, items: cartItems };
+        const response = await fetch(`${apiUrl}/api/orders`,
+          {        
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify(orderData),
+          }
+        );
+        if (!response.ok) {
+          throw new Error('Erro ao registrar o pedido');
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+
+    }
+    catch (error) {
+        console.error(error);
+        return Promise.reject('Erro ao enviar o pedido');
     }
 }
